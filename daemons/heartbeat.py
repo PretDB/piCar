8cf9486df3d617db575566b25589753bab7542e2
Pret D.B <pret-db@qq.com>
2018-08-22 20:42:46 +0800

    add daemon files runs on rasp

---
+++ daemons/heartbeat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import socket
+import time
+import json
+
+
+# address = ('<broadcast>', 6868)
+address = ('<broadcast>', 6868)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+testS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+testS.connect(('8.8.8.8', 80))
+localIP = testS.getsockname()[0]
+testS.close()
+s.bind(('', 9999))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+heartbeatPackage = {'FromIP':localIP,'FromID':0, 'FromRole':'car','Type':'heartbeat','Msg':None}
+
+dataRaw = json.dumps(heartbeatPackage)
+a = 0
+while True:
+    a = a + 1
+    dataByte = dataRaw.encode('utf-8')
+    s.sendto(dataByte, address)
+    print(a, dataByte)
+    time.sleep(5)
+
