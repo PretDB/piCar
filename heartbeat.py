1c6fd14a7b7bfddc005a76a04be6bc13f3c6186a
PretDB <pret-db@pret-page.com>
2018-10-15 16:59:20 +0800

    move map function into heartbeat, and there are two ways to run it, one is argument free for release running, the other one run with an arbitary argument for example "jjj" for debug mode

--- heartbeat.py
+++ heartbeat.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+import sys
 import socket
 import time
 import json
 import spidev
 
+isDebug = len(sys.argv) > 1
+
+fieldX = 1
+fieldY = 1
+lastLoc = (0, 0)
+
+if isDebug:
+    import random
+else:
+    fieldX = 6
+    fieldY = 4
 
 # Network Initializations
 address = ('<broadcast>', 6868)
@@ -29,30 +41,48 @@ heartbeatCount= 0
 c = 0
 
 def GetLoc():
-    raw = bytes(spi.readbytes(128))
-    start = raw.rindex(b'^')
-    end = raw.rindex(b'$')
-    if start < end:
-        m = bytes(raw[start : end])
-        msg = m.decode(encoding='ascii')
+    global lastLoc
+    global fieldX
+    global fieldY
 
-        tagIndex = msg.index('T')
-        tag = msg[tagIndex + 1]
-        if str(id) == str(msg[tagIndex + 1]):
-            xIndex = msg.index('X')
-            yIndex = msg.index('Y')
-            xString = msg[xIndex + 1 : yIndex]
-            yString = msg[yIndex + 1 : ]
-            xVal = float(xString)
-            yVal = float(yString)
-            loc = {'tag' : tag, 'X' : xVal, 'y' : yVal}
-            return loc
-        else:
-            print('tag not right')
+    x = 0.0
+    y = 0.0
+    t = 0
+    if not isDebug:
+        try:
+            raw = bytes(spi.readbytes(128))
+            start = raw.rindex(b'^')
+            end = raw.rindex(b'$')
+            if start < end:
+                m = bytes(raw[start : end])
+                msg = m.decode(encoding='ascii')
+    
+                tagIndex = msg.index('T')
+                tag = msg[tagIndex + 1]
+                if str(id) == str(msg[tagIndex + 1]):
+                    xIndex = msg.index('X')
+                    yIndex = msg.index('Y')
+                    xString = msg[xIndex + 1 : yIndex]
+                    yString = msg[yIndex + 1 : ]
+                    xVal = float(xString)
+                    yVal = float(yString)
+                    x = xVal / fieldX
+                    y = yVal / fieldY
+                    t = tag
+                else:
+                    print('Error in func GetLoc: tag not right, use last location')
+            else:
+                print('Error in func GetLoc: message string not valid, use last location')
+        except ValueError:
             return None
+
     else:
-        print('message string not valid')
-        return None
+        x = round( random.random(), 2)
+        y = round( random.random(), 2)
+    lastLoc = (x, y)
+    loc = {'tag' : t, 'X' : lastLoc[0], 'Y' : lastLoc[1]}
+    return loc
+
 
 while True:
     heartbeatCount= heartbeatCount + 1
@@ -68,7 +98,9 @@ while True:
     dataByte = dataRaw.encode('utf-8')
 
     s.sendto(dataByte, address)
-    print(heartbeatCount, dataByte)
+    print(time.ctime(), 'count: ', heartbeatCount, )
+    print('Data: ', dataByte)
+    print('')
 
     time.sleep(0.2)
 
