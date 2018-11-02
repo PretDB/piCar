99337973aa1ef718fc5bd74f79db168b19a1eec6
Pret D.B <pret-db@pret-page.com>
2018-11-02 16:23:02 +0800

    add cali

--- qmc.py
+++ qmc.py
@@ -5,9 +5,29 @@ import math
 
 
 class QMC:
-    def __init__(self):
+    def __init__(self, readCali):
         self.address = 0x0d
         self.handler = wiringpi.wiringPiI2CSetup( self.address )
+
+        self.xoff = 0
+        self.yoff = 0
+        self.zoff = 0
+        self.xgain = 1
+        self.ygain = 1
+        self.zgain = 1
+        if readCali == True:
+            try:
+                f = open('cali.conf', 'r')
+                self.xoff = int( f.readline() )
+                self.yoff = int( f.readline() )
+                self.zoff = int( f.readline() )
+                self.xgain = int( f.readline() )
+                self.ygain = int( f.readline() )
+                self.zgain = int( f.readline() )
+            except FileNotFoundError:
+                f = open('cali.conf', 'w')
+                f.flush()
+                f.close()
 
         self.reset()
 
@@ -49,9 +69,15 @@ class QMC:
         tmp[3] = ctypes.c_byte( dataRaw[3] )
         tmp[5] = ctypes.c_byte( dataRaw[5] )
 
+        # Assembly data
         data[0] = tmp[1].value << 8 | tmp[0].value
         data[1] = tmp[3].value << 8 | tmp[2].value
         data[2] = tmp[5].value << 8 | tmp[4].value
+
+        # Cali
+        data[0] = self.xgain * ( data[0] + self.xoff )
+        data[1] = self.ygain * ( data[1] + self.yoff )
+        data[2] = self.zgain * ( data[2] + self.zoff )
 
         return data
 
@@ -60,17 +86,26 @@ class QMC:
         angle = math.atan2(xyz[1], xyz[0]) * 180 / math.pi + 180
         return angle
 
-    def cali( self ):
+    def cali( self, write ):
         x = list()
         y = list()
+        z = list()
 
         for i in range(2000):
             xx, yy, zz = self.readMag()
             x.append(xx)
             y.append(yy)
+            z.append(zz)
             time.sleep(0.005)
 
         xoff = - ( ( max( x ) - min( x ) ) / 2 )
         yoff = - ( ( max( y ) - min( y ) ) / 2 )
+        zoff = - ( ( max( z ) - min( z ) ) / 2 )
+
+        if write == True:
+            f = open( 'cali.conf' )
+            f.write(str( xoff ) + '\n' )
+            f.write(str( yoff ) + '\n' )
+            f.close()
 
         return xoff, yoff
