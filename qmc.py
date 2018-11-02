2892b59ce461455710f499faf20b8837c2687789
Pret D.B <pret-db@pret-page.com>
2018-11-02 14:35:10 +0800

    base functions of qmc

--- qmc.py
+++ qmc.py
@@ -1,3 +1,25 @@
+import wiringpi
+import ctypes
+
+
 class QMC:
     def __inti__(self):
-        self.address = 0x0dclass QMC:
+        self.address = 0x0d
+        self.handler = wiringpi.wiringPiI2CSetup( self.address )
+
+    def writeReg( self, addr, val ):
+        return wiringpi.wiringPiI2CWriteReg8( self.handler, addr, val )
+
+    def readReg( self, addr ):
+        raw = wiringpi.wiringPiI2CReadReg8( self.handler, addr )
+        return raw
+
+    def readMag_Raw( self ):
+        dataRaw = (0, 0, 0, 0, 0, 0)
+        ready_raw = self.readReg( 0x06 )
+
+        if ready_raw & 0x01 == 0x01:
+            for i in range( 6 ):
+                dataRaw[i] = self.readReg( i )
+
+        return dataRaw
