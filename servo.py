75a8e3305e2ea42866455215adc770127baedbeb
Pret D.B <pret-db@pret-page.com>
2018-11-05 19:42:21 +0800

    add servo driver

---
+++ servo.py
@@ -0,0 +1,21 @@
+import pca
+
+class Servo:
+    def __init__( self, pca, channel, maxAngle = 180, minTime, maxTime ):
+        self.pca = pca
+        self.channel = channel
+
+        self.maxAngle = maxAngle
+        self.minTime = minTime
+        self.maxTime = maxTime
+        pca.setChannelValue_time_ms( channel, 0, minTime )
+        self.angle = 0
+
+        return
+
+    def setAngle( self, angle ):
+        if angle <= self.maxAngle:
+            time = angle / self.maxAngle * ( self.maxTime - self.minTime )
+            self.pca.setChannelValue_time_ms( self.channel, 0, time )
+
+        return
