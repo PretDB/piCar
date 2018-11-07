3fb35c6fb54c7ca3db5075f9827275cd9448e1d5
Pret D.B <pret-db@pret-page.com>
2018-11-07 14:00:00 +0800

    add mcp.py

---
+++ mcp.py
@@ -0,0 +1,32 @@
+import wiringpi
+
+class MCP:
+    def __init__( self, add, ss ):
+        self.ioDirectReg_A = 0x00
+        self.ioDirectReg_B = 0x01
+        self.iPolarityReg_A = 0x02
+        self.iPolarityReg_B = 0x03
+        self.gpIntEn_A = 0x04
+        self.gpIntEn_B = 0x05
+        self.defVal_A = 0x06
+        self.defVal_B = 0x07
+        self.intCon_A = 0x08
+        self.intCon_B = 0x09
+        self.ioCon = 0x0A
+        self.gpPU_A = 0x0C
+        self.gpPU_B = 0x0D
+        self.intFlag_A = 0x0E
+        self.intFlag_B = 0x0F
+        self.intCap_A = 0x10
+        self.intCap_B = 0x11
+        self.gpio_A = 0x12
+        self.gpio_B = 0x13
+        self.outLatch_A = 0x14
+        self.outLatch_B = 0x15
+
+        self.address = add
+        self.ss =  ss
+        self.mode = 0xFFFF      # Default IO mode is all input
+        self.output = 0x0000    # Default output state is all off
+        self.pullUp = 0x0000    # Default pull-up state is all off
+        self.invert = 0x0000    # Default input invertion state is not
