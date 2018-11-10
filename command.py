52f96fd95d8bcc4ec7d10e42ba376b1fab1fefb5
Pret D.B <pret-db@pret-page.com>
2018-11-10 16:35:27 +0800

    add definations of other functions

--- command.py
+++ command.py
@@ -3,6 +3,7 @@ from enum import Enum, unique
 
 @unique
 class Command(Enum):
+    Non = 100
     Stop = 0
     Forward = 1
     Backward = 2
@@ -10,3 +11,10 @@ class Command(Enum):
     RightShift = 4
     LeftRotate = 5
     RightRotate = 6
+    IR = 10
+    Sonic = 11
+    Light = 12
+    HumanDetect = 13
+    FireDetect = 14
+    SoundDetect = 15
+    Track = 1000
