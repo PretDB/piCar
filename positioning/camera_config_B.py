d4d58ca83236f857d62f7147980b70fb03459c61
Pret D.B <pret-db@pret-page.com>
2018-11-11 11:42:34 +0800

    remove positionning dir which has never been used

--- positioning/camera_config_B.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-camera_matrix = np.array([[414.5016,   0.0000, 320.3094],
-                          [0.0000, 413.9584, 225.5390],
-                          [0.0000,   0.0000,   1.0000]
-                          ]
-                         )
-camera_distortion = np.array([[0.0108, -0.0086, -0.0020, -0.0046]])
-cameraID = 'B'
