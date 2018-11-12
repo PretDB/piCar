d98ae583c11fb98a7815d7ac694f175e6c5158c3
Pret D.B <pret-db@pret-page.com>
2018-11-12 22:18:52 +0800

    use mcp pin in motor.py instead of rasp pin

--- motor.py
+++ motor.py
@@ -2,10 +2,11 @@ import wiringpi
 import pca
 
 class Motor:
-    def __init__( self, pcaInstance, pwmChannel, invPin ):
+    def __init__( self, pcaInstance, pwmChannel, mcpInstance, invPin ):
         self.pwmChannel = pwmChannel
         self.invPin = invPin
         self.pca = pcaInstance
+        self.mcp = mcpInstance
 
         return
 
@@ -13,10 +14,10 @@ class Motor:
     # inverse = True or 1 for inverse
     def rotate(self, inverse, speed):
         if not inverse:
-            wiringpi.digitalWrite(self.invPin, wiringpi.LOW)
+            self.mcp.digitalWrite(self.invPin, 0)
             self.pca.setChannelValue_ratio(self.pwmChannel, 0, 1 - speed)
         else:
-            wiringpi.digitalWrite(self.invPin, wiringpi.HIGH)
+            self.mcp.digitalWrite(self.invPin, 1)
             self.pca.setChannelValue_ratio(self.pwmChannel, 0, speed)
 
         return
