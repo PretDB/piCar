f2b41212b2682ae7c31e608f92841d0896dce095
PretDB <pret-db@pret-page.com>
2018-12-31 20:05:16 +0800

    add sonic driver ( without thread ).

---
+++ sonic.py
@@ -0,0 +1,40 @@
+import wiringpi
+import pca
+import mcp
+import command
+import servo
+
+
+class Sonic:
+    def __init__(self, pca, channel, mcp, echo, trig):
+        self.servo = servo.Servo(pca, channel)
+        self.mcp = mcp
+        self.trigPin = trig
+        self.echoPin = echo
+
+        self.mcp.pinMode(self.trigPin, 0)
+        self.mcp.pinMode(self.echoPin, 1)
+
+        pass
+
+    def readCMHere(self):
+        self.mcp.digitalWrite(self.trigPin, wiringpi.HIGH)
+        time.sleep(0.00001)
+        self.mcp.digitalWrite(self.trigPin, wiringpi.LOW)
+
+        while self.mcp.digitalRead(self.echoPin) == 0:
+            pass
+        startTime = time.time()
+
+        while self.mcp.digitalRead(self.echoPin) == 1:
+            pass
+        endTime = time.time()
+
+        t = endTime - startTime
+        d = t * 343.0 / 2 * 100
+
+        return d
+
+    def setAngle(self, angle):
+        self.servo.setAngle(angle)
+
